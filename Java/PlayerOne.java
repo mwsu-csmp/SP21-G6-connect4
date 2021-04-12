604778728efc60168ea7838ac34a4c35c9ed0058
jimmy <jkozminski@missouriwestern.edu>
2021-04-12 14:08:16 -0500

    pojo class of PlayerOne

---
+++ Java/PlayerOne.java
@@ -0,0 +1,14 @@
+public class PlayerOne {
+    private String userName;
+    private int TotalWins;
+    public PlayerOne (String userName, int Wins ){
+        this.userName = userName;
+        this.TotalWins = Wins;
+    }
+    public int getTotalWins(){
+        return TotalWins;
+    }
+    public void setTotalWins(int Wins){
+        TotalWins = Wins;
+    }
+}
