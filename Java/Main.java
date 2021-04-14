6f910a83af3924bd69dc4960b7a85e5d3bb943aa
Jesse-Miller <jmiller87@missouriwestern.edu>
2021-04-14 15:42:48 -0500

    added sample code

---
+++ Java/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+        var root = new FlowPane();
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
